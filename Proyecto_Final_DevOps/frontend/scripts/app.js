2b315756589c24f39c6cbc0cd718d5e936f6b429
AdonisMBuret <2021239@itla.edu.do>
2025-12-12 00:25:00 -0400

    Cambios de carperta

--- Proyecto_Final_DevOps/frontend/scripts/app.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("sendBtn").addEventListener("click", async () => {
-    const name = document.getElementById("nameInput").value;
-
-    const response = await fetch("http://localhost:3000/api/hello?name=" + name);
-    const data = await response.json();
-
-    document.getElementById("response").innerText = data.message;
-});
---
+++ backend/tests/integration/api.test.js
@@ -0,0 +1,14 @@
+const request = require("supertest");
+const express = require("express");
+const routes = require("../routes");
+
+const app = express();
+app.use(express.json());
+app.use("/api", routes);
+
+test("GET /api/hello responde correctamente", async () => {
+    const res = await request(app).get("/api/hello?name=Test");
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toBe("Hola Test, bienvenido a la app DevOps!");
+});
